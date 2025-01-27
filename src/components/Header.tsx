5688120fd06d10d2b38456628a272a03c6053a6d
Felipe Sepulveda <felipe.sepulveda@dnb.no>
2025-01-27 10:24:01 +0100

    added login modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import LoginModal from './LoginModal';
 
-const Header: React.FC = () => {
+export default function ButtonAppBar() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">
-          Family Recipes
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: '#0D47A1' }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Family Recipes
+          </Typography>
+          <Button color="inherit" onClick={handleClickOpen}>Login</Button>
+        </Toolbar>
+      </AppBar>
+      <LoginModal open={open} onClose={handleClose} />
+    </Box>
   );
-};
-
-export default Header;
+}
---
+++ src/components/LoginModal.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+interface LoginModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export default function LoginModal({ open, onClose }: LoginModalProps) {
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Login</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Please enter your login details.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="nickname"
+          label="Nickname"
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          margin="dense"
+          id="password"
+          label="Password"
+          type="password"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={onClose} color="primary">
+          Login
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
