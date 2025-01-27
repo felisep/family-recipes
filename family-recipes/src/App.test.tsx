e06bc80c191d3d75330a0f1e0c661012684b99c5
Felipe Sepulveda <felipe.sepulveda@dnb.no>
2025-01-27 09:02:35 +0100

    setup folder structure

---
+++ family-recipes/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from './pages/Home';
+
+test('renders header with title Family Recipes', () => {
+  render(<Home />);
+  const headerElement = screen.getByText(/Family Recipes/i);
+  expect(headerElement).toBeInTheDocument();
+});
---
+++ family-recipes/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import '../App.css';
+import Header from '../components/Header';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      {/* Other content */}
+    </div>
+  );
+};
+
+export default Home;
