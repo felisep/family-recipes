d6941cb02795291d1c01d049c5095520c61bbf8a
Felipe Sepulveda <Felipe.Sepulveda@dnb.no>
2026-08-01 11:04:15 +0200

    change home and sort list

--- src/components/RecipeList/RecipeTable.tsx
+++ src/components/RecipeList/RecipeTable.tsx
@@ -2,27 +2,20 @@ import type { Recipe } from "../../types/recipe";
 import RecipesRow from "./RecipesRow";
 import "./RecipeTable.css";
 
-const RecipeHeader = () => {
-	return (
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Description</th>
-			</tr>
-		</thead>
-	);
-};
-
 const RecipeTable = ({ recipes }: { recipes: Recipe[] }) => {
+	const sortedRecipes = [...recipes].sort((a, b) => a.name.localeCompare(b.name));
+	
 	return (
+		<>
+		<h1>Name</h1>
 		<table>
-			<RecipeHeader />
 			<tbody>
-				{recipes.map((recipe) => (
-					<RecipesRow recipe={recipe} key={recipe.name} />
-				))}
-			</tbody>
+					{sortedRecipes.map((recipe) => (
+						<RecipesRow recipe={recipe} key={recipe.name} />
+					))}
+				</tbody>
 		</table>
+		</>
 	);
 };
 
