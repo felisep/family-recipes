cf04c4fcf4f300bc621b1ac83ef12265dd49e442
Felipe Sepulveda <Felipe.Sepulveda@dnb.no>
2026-08-03 08:16:16 +0200

    refactor About component text for clarity and conciseness

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,8 +4,8 @@ const About = () => {
 			<img src="" alt="" />
 			<h1>Om familie oppskrifter</h1>
 			<p>Siden er laget for å dele oppskrifter med familie og venner.</p>
-			Du kan legge til dine egne oppskrifter, se andres oppskrifter, og justere
-			mengden ingredienser basert på antall personer du lager mat til.
+			Du kan justere mengden ingredienser basert på antall personer du lager mat
+			til.
 		</div>
 	);
 };
