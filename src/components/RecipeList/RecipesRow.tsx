dd41484c7cb77cd5a6ab230d16b26662bf8d3b35
Felipe Sepulveda <Felipe.Sepulveda@dnb.no>
2026-08-15 15:12:51 +0200

    Refactor RecipeTable styles and add RecipesRow component with basic structure

--- src/components/RecipeList/RecipesRow.tsx
+++ src/components/RecipeList/RecipesRow.tsx
@@ -1,15 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import type { RecipesRowProps } from "../../types/recipe";
+import "./RecipesRow.css";
 
 const RecipesRow = ({ recipe }: RecipesRowProps) => {
 	const navigate = useNavigate();
 	return (
-		<tr
-			onClick={() => navigate(`/recipe/${recipe.id}`)}
-			style={{ cursor: "pointer" }}
-		>
-			<td>{recipe.name}</td>
-		</tr>
+		<table>
+			<tr
+				onClick={() => navigate(`/recipe/${recipe.id}`)}
+				style={{ cursor: "pointer" }}
+				className="recipe-row"
+			>
+				<td>{recipe.name}</td>
+			</tr>
+		</table>
 	);
 };
 
